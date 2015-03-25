3bc14f928f9f9f3bb4e5f87723543efd19fe1ce9
Dr. Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2015-03-25 17:02:42 +0100

    Fixed "Unknown material type" not being translated.

--- module/VuFind/src/VuFind/RecordDriver/SolrDefault.php
+++ module/VuFind/src/VuFind/RecordDriver/SolrDefault.php
@@ -1164,6 +1164,7 @@ class SolrDefault extends AbstractBase
 	    "Inhaltstext"               => "contents",
 	    "Verlagsinformation"        => "publisher information",
 	    "Ausführliche Beschreibung" => "detailed description",
+	    "Unbekanntes Material"      => "unknown material type",
         ];
         $map_to_french = [
             "Inhaltsverzeichnis"        => "contenu",
@@ -1173,6 +1174,7 @@ class SolrDefault extends AbstractBase
 	    "Inhaltstext"               => "contenu du texte",
 	    "Verlagsinformation"        => "informations editeur",
 	    "Ausführliche Beschreibung" => "la pleine description",
+	    "Unbekanntes Material"      => "matériau inconnu",
         ];
 
 	// Determine language code:
